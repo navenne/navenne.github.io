f97ce0d9e46525536dc60b816e3e8f3b248166b4
navenne <a20hirila@iesgrancapitan.org>
2022-09-14 13:31:09 +0200

    upload new portfolio

---
+++ js/nav.js
@@ -0,0 +1,21 @@
+/**
+ * @author Laura Hidalgo Rivera
+ */
+
+{
+  document.addEventListener("DOMContentLoaded", function () {
+    const nav = document.querySelector(".mobile_menu");
+    const blur = document.querySelector(".content_wrapper");
+    document
+      .querySelector(".menu")
+      .addEventListener("click", function () {
+        nav.style.width = "250px";
+        blur.classList.toggle("blur_bg");
+      });
+
+    document.getElementById("close").addEventListener("click", function () {
+      nav.style.width = "0";
+      blur.classList.toggle("blur_bg");
+    });
+  });
+}
